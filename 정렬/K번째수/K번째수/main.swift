c25a297a847a16dbb5d6baa60f52a5598727d7f2
부엉이 <o.o.wl.soft@gmail.com>
2019-07-07 20:24:47 +0900

    feat: Finish K번째 수 찾기

--- 정렬/K번째수/K번째수/main.swift
+++ 정렬/K번째수/K번째수/main.swift
@@ -9,7 +9,16 @@
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    
-    return []
+
+    var result = [Int]()
+    for command in commands {
+        let start = command[0]
+        let end = command[1]
+        let k = command[2]
+        if start == end { result.append( array[start-1]); continue }
+        let range = start-1..<end
+        result.append(array[range].sorted(by:<)[k-1])
+    }
+    return result
 }
 
