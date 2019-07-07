5aa0d67c17c228a1fd37b3d179471c8d922aa176
부엉이 <o.o.wl.soft@gmail.com>
2019-07-07 20:58:55 +0900

    feat: Finish 프린터

--- 스택:큐/프린터/프린터/main.swift
+++ 스택:큐/프린터/프린터/main.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
-func solution(_ priorities:[Int], _ location:Int) -> Int {
-    return 0
+func solution(_ priorities: [Int], _ location: Int) -> Int {
+    var queue = priorities
+    var output = [Int]()
+    var location = location
+    
+    while !queue.isEmpty {
+        if location == 0 {
+            if queue.first! == queue.max()! { return output.count+1 }
+            location = queue.count
+        }
+        let first = queue.removeFirst(); location-=1
+        queue.filter { $0 > first }.count > 0 ? queue.append(first) : output.append(first)
+    }
+    return output.count+1
 }
