fbfa8ef42470831775983977543f1076d4774ea7
부엉이 <o.o.wl.soft@gmail.com>
2019-07-10 21:48:30 +0900

    test: Add test

--- 연습문제/수박수박수박수박수박수/SolutionTests/SolutionTests.swift
+++ 연습문제/수박수박수박수박수박수/SolutionTests/SolutionTests.swift
@@ -10,6 +10,57 @@ import XCTest
 
 class SolutionTests: XCTestCase {
 
+    func test1() {
+        //Given
+        let input = 10
+        let expected = "수박수박수박수박수박"
+       
+        //When
+        let result = solution(input)
+        
+        //Then
+        XCTAssertEqual(result, expected)
+        
+    }
+    
+    func test2() {
+        //Given
+        let input = 11
+        let expected = "수박수박수박수박수박수"
+        
+        //When
+        let result = solution(input)
+        
+        //Then
+        XCTAssertEqual(result, expected)
+        
+    }
+    
+    func test3() {
+        //Given
+        let input = 3
+        let expected = "수박수"
+        
+        //When
+        let result = solution(input)
+        
+        //Then
+        XCTAssertEqual(result, expected)
+        
+    }
+    
+    func test4() {
+        //Given
+        let input = 4
+        let expected = "수박수박"
+        
+        //When
+        let result = solution(input)
+        
+        //Then
+        XCTAssertEqual(result, expected)
+        
+    }
    
 
 }
--- 연습문제/수박수박수박수박수박수/수박수박수박수박수박수/main.swift
+++ 연습문제/수박수박수박수박수박수/수박수박수박수박수박수/main.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 func solution(_ n:Int) -> String {
-    return
+    return ""
 }
