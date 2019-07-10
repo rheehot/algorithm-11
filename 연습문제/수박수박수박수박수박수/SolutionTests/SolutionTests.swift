85cb32f2984f540eb2928a456343f535a069b765
부엉이 <o.o.wl.soft@gmail.com>
2019-07-10 21:45:48 +0900

    Start 수박수박수박수박수?

---
+++ 연습문제/수박수박수박수박수박수/SolutionTests/SolutionTests.swift
@@ -0,0 +1,15 @@
+//
+//  SolutionTests.swift
+//  SolutionTests
+//
+//  Created by 이동영 on 10/07/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import XCTest
+
+class SolutionTests: XCTestCase {
+
+   
+
+}
---
+++ 연습문제/수박수박수박수박수박수/수박수박수박수박수박수/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  수박수박수박수박수박수
+//
+//  Created by 이동영 on 10/07/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import Foundation
+
+func solution(_ n:Int) -> String {
+    return
+}
