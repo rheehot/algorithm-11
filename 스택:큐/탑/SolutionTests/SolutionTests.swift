049e70e50ec0ba77ed861a52ee4f3d860262eda5
부엉이 <o.o.wl.soft@gmail.com>
2019-07-11 16:22:20 +0900

    feat: Start 탑

---
+++ 스택:큐/탑/SolutionTests/SolutionTests.swift
@@ -0,0 +1,30 @@
+//
+//  SolutionTests.swift
+//  SolutionTests
+//
+//  Created by 이동영 on 11/07/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+import XCTest
+
+class SolutionTests: XCTestCase {
+    
+    func test01() {
+        let input = [6,9,5,7,4]
+        let expected = [0,0,2,2,4]
+        
+        
+    }
+    
+    func test02() {
+        
+    }
+    
+    func test03() {
+        
+    }
+  
+    [3,9,9,3,5,7,2]    [0,0,0,3,3,3,6]
+    [1,5,3,6,7,6,5]    [0,0,2,0,0,5,6]
+}
---
+++ 스택:큐/탑/탑/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  탑
+//
+//  Created by 이동영 on 11/07/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+import Foundation
+
+func solution(_ heights:[Int]) -> [Int] {
+    return []
+}
