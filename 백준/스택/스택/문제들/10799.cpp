0c7c61e1e8383a1192308f9b07cb2f4330561606
부엉이 <o.o.wl.soft@gmail.com>
2019-10-19 05:49:08 +0900

    feat: Solve 쇠막대기 10799

--- 백준/스택/스택/문제들/10799.cpp
+++ 백준/스택/스택/문제들/10799.cpp
@@ -1,11 +1,10 @@
 //
-//  main.cpp
+//  10799.cpp
 //  스택
 //
 //  Created by 이동영 on 19/10/2019.
 //  Copyright © 2019 부엉이. All rights reserved.
 //
-
 #include <iostream>
 #include <string>
 #include <stack>
