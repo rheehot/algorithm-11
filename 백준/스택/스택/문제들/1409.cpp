9d7955ddd380072b124ab749c1841979ecc28365
부엉이 <o.o.wl.soft@gmail.com>
2019-10-21 01:54:16 +0900

    Solve 1409 에디터

--- 백준/스택/스택/문제들/1409.cpp
+++ 백준/스택/스택/문제들/1409.cpp
@@ -15,55 +15,55 @@ using namespace std;
 
 // https://www.acmicpc.net/problem/1406
 
-//int main(int argc, const char * argv[]) {
-//    
-//    char chars[600000];
-//    int cnt;
-//    stack<char> left,right;
-//    
-//    scanf("%s", chars);
-//    scanf("%d", &cnt);
-//    for(int i=0; i<(int)strlen(chars);i++) {
-//        left.push(chars[i]);
-//    }
-//    
-//    char instruction;
-//    
-//    while(cnt--) {
-//        scanf(" %c", &instruction);
-//        switch (instruction) {
-//            case 'L':
-//                if(left.empty()) { break; }
-//                right.push(left.top());
-//                left.pop();
-//                break;
-//            case 'D':
-//                if(right.empty()) { break; }
-//                left.push(right.top());
-//                right.pop();
-//                break;
-//            case 'B':
-//                if(left.empty()) { break; }
-//                left.pop();
-//                break;
-//            case 'P':
-//                char c;
-//                scanf(" %c", &c);
-//                left.push(c);
-//                break;
-//            default:
-//                break;
-//        }
-//    }
-//    
-//    while(!left.empty()) {
-//        right.push(left.top());
-//        left.pop();
-//    }
-//    
-//    while(!right.empty()) {
-//        printf("%c", right.top());
-//        right.pop();
-//    }
-//    
-//}
+int main_1409(int argc, const char * argv[]) {
+    
+    char chars[600000];
+    int cnt;
+    stack<char> left,right;
+    
+    scanf("%s", chars);
+    scanf("%d", &cnt);
+    for(int i=0; i<(int)strlen(chars);i++) {
+        left.push(chars[i]);
+    }
+    
+    char instruction;
+    
+    while(cnt--) {
+        scanf(" %c", &instruction);
+        switch (instruction) {
+            case 'L':
+                if(left.empty()) { break; }
+                right.push(left.top());
+                left.pop();
+                break;
+            case 'D':
+                if(right.empty()) { break; }
+                left.push(right.top());
+                right.pop();
+                break;
+            case 'B':
+                if(left.empty()) { break; }
+                left.pop();
+                break;
+            case 'P':
+                char c;
+                scanf(" %c", &c);
+                left.push(c);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    while(!left.empty()) {
+        right.push(left.top());
+        left.pop();
+    }
+    
+    while(!right.empty()) {
+        printf("%c", right.top());
+        right.pop();
+    }
+    
+}
