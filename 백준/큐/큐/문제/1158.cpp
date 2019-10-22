30ed7d1fb02742b7caed85bf477778578614dab0
부엉이 <o.o.wl.soft@gmail.com>
2019-10-22 12:42:12 +0900

    Solve 1158 조세퍼스문제

---
+++ 백준/큐/큐/문제/1158.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  큐
+//
+//  Created by 이동영 on 21/10/2019.
+//  Copyright © 2019 부엉이. All rights reserved.
+//
+
+#include <iostream>
+#include "queue"
+
+using namespace std;
+
+int main_1158(int argc, const char * argv[]) {
+    
+    queue<int> q;
+    
+    int num;
+    int interval;
+    scanf("%d %d", &num, &interval);
+    interval--;
+    
+    for (int i=1;i<=num;i++){
+        q.push(i);
+    }
+    
+    int cnt = 0;
+    printf("<");
+    while (q.size() != 1) {
+        if (interval == cnt) {
+            printf("%d, ",q.front());
+            q.pop();
+            cnt = 0;
+        } else {
+            q.push(q.front());
+            q.pop();
+            cnt++;
+        }
+    }
+    printf("%d>",q.front());
+    return 0;
+}
